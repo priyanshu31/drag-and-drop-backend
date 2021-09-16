99777ed44b988b34df4795e788c9afa0134e9cb5
priyanshu31 <priyanshu.31082003@gmail.com>
2021-09-17 02:00:49 +0530

    Added cors to make this server public

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv").config();
 const dataFormat = require('./models/dataFormat');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 80;
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true})); 
 
@@ -28,35 +30,38 @@ app.get('/data', async (req, res) => {
     const req_value = parseInt(req.query.value);
     // data = await dataFormat.find({value: req_value});
 
-    if(req.query.operator === '<') {
-        data = await dataFormat.find({
-            $and: [
-                {
-                    char: req.query.alphabet
-                },
-                {
-                    value: {
-                        $lt: req_value
+    const fetchData = async () => {
+        if(req.query.operator === '<') {
+            data = await dataFormat.find({
+                $and: [
+                    {
+                        char: req.query.alphabet
+                    },
+                    {
+                        value: {
+                            $lt: req_value
+                        }
                     }
-                }
-            ]
-        }).lean();
-    }
-    else {
-        data = await dataFormat.find({
-            $and: [
-                {
-                    char: req.query.alphabet
-                }, 
-                {
-                    value: {
-                        $gt: req_value
+                ]
+            });
+        }
+        else {
+            data = await dataFormat.find({
+                $and: [
+                    {
+                        char: req.query.alphabet
+                    }, 
+                    {
+                        value: {
+                            $gt: req_value
+                        }
                     }
-                }
-            ]
-        }).lean();
+                ]
+            });
+        }
     }
-    
+
+    await fetchData();
     console.log(data);
 
     return res.status(200).json(data);
